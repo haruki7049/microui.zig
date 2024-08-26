ea572d10c504aa1e39e7c17a8c9ccc1db1a1ec99
haruki7049 <tontonkirikiri@gmail.com>
2024-08-26 16:21:44 +0900

    style: Rename mu_* to *

--- src/default_styles.zig
+++ src/default_styles.zig
@@ -1,6 +1,6 @@
 const types = @import("./types.zig");
 
-pub const unclipped_rect: types.mu_Rect = types.mu_Rect {
+pub const unclipped_rect: types.Rect = types.Rect {
     .x = 0,
     .y = 0,
     .width = 0x1000000,
--- src/types.zig
+++ src/types.zig
@@ -1,120 +1,137 @@
-pub const mu_Font = undefined;
-pub const mu_Id = usize;
-pub const mu_Real = f64;
+pub const Font = undefined;
+pub const Id = usize;
+pub const Real = f64;
 
-pub const mu_PoolItem = struct {
-    id: mu_Id,
+pub const PoolItem = struct {
+    id: Id,
     last_update: isize,
 };
 
-pub const mu_BaseCommand = struct {
+pub const BaseCommand = struct {
     type: isize,
     size: isize,
 };
 
-pub const mu_ClipCommand = struct {
-    base: mu_BaseCommand,
-    rect: mu_Rect,
+pub const ClipCommand = struct {
+    base: BaseCommand,
+    rect: Rect,
 };
 
-pub const mu_RectCommand = struct {
-    base: mu_BaseCommand,
-    rect: mu_Rect,
-    color: mu_Color,
+pub const RectCommand = struct {
+    base: BaseCommand,
+    rect: Rect,
+    color: Color,
 };
 
-pub const mu_TextCommand = struct {
-    base: mu_BaseCommand,
-    font: mu_Font,
-    pos: mu_Vec2,
-    color: mu_Color,
+pub const TextCommand = struct {
+    base: BaseCommand,
+    font: Font,
+    pos: Vec2,
+    color: Color,
     str: [] u8,
 };
-pub const mu_Vec2 = struct {
+pub const Vec2 = struct {
     x: isize,
     y: isize,
 };
 
-pub const mu_Rect = struct {
+pub const Rect = struct {
     x: isize,
     y: isize,
     width: isize,
     height: isize,
 };
 
-pub const mu_Color = struct {
+pub const Color = struct {
     r: u8,
     g: u8,
     b: u8,
     a: u8,
 };
 
-pub const mu_Command = union {
-    type: isize,
-    base: mu_BaseCommand,
-    jump: mu_JumpCommand,
-    clip: mu_ClipCommand,
-    rect: mu_RectCommand,
-    text: mu_TextCommand,
-    icon: mu_IconCommand,
+pub const ColorSet = struct {
+    text_color: Color,
+    border_color: Color,
+    windowbg_color: Color,
+    titlebg_color: Color,
+    titletext_color: Color,
+    panelbg_color: Color,
+    button_color: Color,
+    buttonhover_color: Color,
+    buttonfocus_color: Color,
+    base_color: Color,
+    basehover_color: Color,
+    basefocus_color: Color,
+    scrollbase_color: Color,
+    scrollthumb_color: Color,
 };
 
-pub const mu_JumpCommand = struct {
-    base: mu_BaseCommand,
+pub const Command = union {
+    type: isize,
+    base: BaseCommand,
+    jump: JumpCommand,
+    clip: ClipCommand,
+    rect: RectCommand,
+    text: TextCommand,
+    icon: IconCommand,
+};
+
+pub const JumpCommand = struct {
+    base: BaseCommand,
     dst: void,
 };
 
-pub const mu_IconCommand = struct {
-    base: mu_BaseCommand,
-    rect: mu_Rect,
-    id: mu_Id,
-    color: mu_Color,
+pub const IconCommand = struct {
+    base: BaseCommand,
+    rect: Rect,
+    id: Id,
+    color: Color,
 };
 
-pub const mu_Container = struct {
-    tail: mu_Command,
-    head: mu_Command,
-    rect: mu_Rect,
-    body: mu_Rect,
-    content_size: mu_Vec2,
-    scroll: mu_Vec2,
+pub const Container = struct {
+    tail: Command,
+    head: Command,
+    rect: Rect,
+    body: Rect,
+    content_size: Vec2,
+    scroll: Vec2,
     zindex: isize,
     open: isize,
 };
 
-pub const mu_Style = struct {
-    font: mu_Font,
-    size: mu_Vec2,
+pub const Style = struct {
+    font: Font,
+    size: Vec2,
     padding: isize,
     spacing: isize,
     indent: isize,
     title_height: isize,
     scrollbar_size: isize,
     thumb_size: isize,
-    colors: [] mu_Color,
+    colors: ColorSet,
 };
 
-pub const mu_Context = struct {
-    text_width: fn (font: mu_Font, str: u8, len: isize) isize,
-    text_height: fn (font: mu_Font) isize,
-    draw_frame: fn (ctx: mu_Context, rect: mu_Rect, colorid: isize) void,
+pub const Context = struct {
+    text_width: fn (font: Font, str: u8, len: isize) isize,
+    text_height: fn (font: Font) isize,
+    draw_frame: fn (ctx: Context, rect: Rect, colorid: isize) void,
 
-    _style: mu_Style,
-    style: mu_Style,
-    hover: mu_Id,
-    focus: mu_Id,
-    last_id: mu_Id,
-    last_rect: mu_Rect,
+    _style: Style,
+    style: Style,
+    hover: Id,
+    focus: Id,
+    last_id: Id,
+    last_rect: Rect,
     last_zindex: isize,
 };
 
-const mu_Mouse = enum {
+const Mouse = enum {
     LEFT,
     RIGHT,
     MIDDLE,
 };
 
-const mu_Key = enum {
+const Key = enum {
     SHIFT,
     CTRL,
     ALT,
